d77345e39351e1e5848e4645bb8198a87125171d
ChristianEysel <ceysmar2109@g.educaand.es>
2025-03-19 14:18:36 +0100

    Primer Commit

---
+++ src/main/java/daw/JuegoDeLaVida.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package daw;
+
+/**
+ *
+ * @author christian
+ */
+public class JuegoDeLaVida {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
