b6a118d3db8d060d6c6aeda755e57bd0881e4ec8
saljc084 <saljc084@g.educaand.es>
2025-03-20 12:47:09 +0100

    Codigo refactorizado

--- src/main/java/daw/Main.java
+++ src/main/java/daw/Main.java
@@ -10,6 +10,70 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        // MENU PRINCIPAL ------------------------------------------------------
+        int opcion = menu();
+
+        Juego generacionAc;
+
+        switch (opcion) {
+            // NUEVA PARTIDA ---------------------------------------------------
+            case 1 -> {
+                System.out.println("Has elegido 'Nueva partida'. COMENZANDO...");
+                // TAMAÑO DEL TABLERO
+                int tamaño = pregTamanio();
+
+                // INICIALIZAMOS EL TABLERO
+                generacionAc = new Juego(tamaño);
+
+                // PORCENTAJE DE CELULAS VIVAS 
+                int porcentaje = pregPorcentaje();
+                int numVivas = (tamaño * porcentaje) / 100;
+
+                // INICIALIZAR LAS CELULAS VIVAS
+                generacionAc.inicioPartida(numVivas);//************************************************************************en proceso
+            }
+            case 2 -> {
+                // CARGAR PARTIDA ----------------------------------------------
+                System.out.println("Estamos en mantenimiento vuelva en un siglo");
+                System.exit(0); // Esto es temporal mientras no se completa
+            }
+            case 3 -> {
+                // SALIR DEL JUEGO ---------------------------------------------
+                System.out.println("Saliendo.......");
+                System.exit(0); // Termina programa
+            }
+            default -> {
+                System.out.println("Esto no deberia salir...");
+            }
+        }
+
+        // LOOP JUGABLE --------------------------------------------------------
+        boolean salidaJuego = false;
+        do {
+            // MENU 2 ------------------------------------------------------------------
+            opcion = menu2();
+
+            if (opcion == 1) {
+                // SIGUIENTE GENERACION --------------------------------------------------------
+
+            } else {
+                // MENU 3 (GUARDAR PARTIDA)-----------------------------------------------------
+                opcion = menu3();
+
+                if (opcion == 1) {
+                    // GUARDAR PARTIDA -------------------------------------------------------------
+                    System.out.println("La funcion está en mantenimiento");
+                }
+
+                System.out.println("Gracias por jugar");
+                salidaJuego = true;
+            }
+
+        } while (!salidaJuego);
+
+    }
+
+    public static int menu() {
         Scanner sc = new Scanner(System.in);
         String menu = """
                        -------------------------------------------------
@@ -20,97 +84,29 @@ public class Main {
                        |            2.- Cargar partida                 |
                        |            3.- Salir                          |
                        -------------------------------------------------
-                      """;
-        boolean salida = true;
-        int opcion = 0;
-// MENU PRINCIPAL --------------------------------------------------------------
+                       Respuesta: """;
+        int opcion;
         do {
-            System.out.println(menu);
-
             try {
+                System.out.print(menu);
                 opcion = sc.nextInt();
-
-                if (opcion > 0 && opcion < 4) {
-                    salida = false;
-                } else {
-                    System.out.println("Intentalo de nuevo tontito");
+                while (opcion < 1 || opcion > 3) {
+                    System.out.println("El dato tiene que ser 1, 2 o 3");
+                    System.out.print(menu);
+                    opcion = sc.nextInt();
                 }
+                break;
             } catch (InputMismatchException ime) {
-                System.out.println("Intentalo de nuevo tontito");
+                System.out.println("El dato tiene que ser numerico");
                 sc.nextLine();
             }
-        } while (salida);
+        } while (true);
+        return opcion;
+    }
 
-        Juego generacionAc;
-                
-        switch (opcion) {
-
-            case 1 -> {
-// NUEVA PARTIDA ---------------------------------------------------------------
-                System.out.println("Has elegido 'Nueva partida'. COMENZANDO...");
-                int tamaño = 0;
-                int porcentaje = 0;
-                salida = true;
-    // TAMAÑO DEL TABLERO
-                do {
-                    //comprobamos que el usuario no la lie
-                    System.out.println("Elige el tamaño del tablero (max 25):");
-                    try {
-                        tamaño = sc.nextInt();
-
-                        if (tamaño < 26) {
-                            salida = false;
-                        } else {
-                            System.out.println("Intentalo de nuevo tontito ya te dije que maximo 25");
-                        }
-                    } catch (InputMismatchException ime) {
-                        System.out.println("Intentalo de nuevo tontito");
-                        sc.nextLine();
-                    }
-                } while (salida);
-                
-                generacionAc = new Juego(tamaño);
-    // PORCENTAJE DE CELULAS VIVAS 
-                salida = true;
-                //comprobamos que el usuario no la lie
-                do {
-                    System.out.println("Ahora elija el porcentaje de celulas vivas:");
-                    try {
-                        porcentaje = sc.nextInt();
-
-                        if (porcentaje < 101) {
-                            salida = false;
-                        } else {
-                            System.out.println("Intentalo de nuevo tontito ya te dije que maximo 25");
-                        }
-                    } catch (InputMismatchException ime) {
-                        System.out.println("Intentalo de nuevo tontito");
-                        sc.nextLine();
-                    }
-                } while (salida);
-                
-                int numVivas = (tamaño*porcentaje)/100;
-                generacionAc.inicioPartida(numVivas);//************************************************************************en proceso
-            }
-            case 2 -> {
-// CARGAR PARTIDA --------------------------------------------------------------
-
-                System.out.println("Estamos en mantenimiento vuelva en un siglo");
-            }
-            case 3 -> {
-// SALIR DEL JUEGO -------------------------------------------------------------
-                System.out.println("Saliendo.......");
-                System.exit(0); // termina programa
-            }
-            default -> {
-                System.out.println("esto no deberia salir...");
-            }
-        }
-
-// LOOP JUGABLE ----------------------------------------------------------------
-        boolean salidaJuego = false;
-        do {
-            String menu2 = """
+    public static int menu2() {
+        Scanner sc = new Scanner(System.in);
+        String menu2 = """
                        -------------------------------------------------
                        |               Juego de la vida                |
                        |               Elija la opcion:                |
@@ -119,36 +115,29 @@ public class Main {
                        |            2.- Terminar partida               |
                        |                                               |
                        -------------------------------------------------
-                      """;
-            salida = true;  // reutilizamos salida
-            opcion = 0;
-            sc.nextLine(); //limpiamos scanner
-            do {
-// MENU 2 ----------------------------------------------------------------------
-                System.out.println(menu2);
-
-                try {
+                       Respuesta: """;
+        int opcion;
+        do {
+            try {
+                System.out.print(menu2);
+                opcion = sc.nextInt();
+                while (opcion < 1 || opcion > 2) {
+                    System.out.println("El dato tiene que ser 1 o 2");
+                    System.out.print(menu2);
                     opcion = sc.nextInt();
-
-                    if (opcion > 0 && opcion < 3) {
-                        salida = false;
-                    } else {
-                        System.out.println("Intentalo de nuevo tontito");
-                    }
-                } catch (InputMismatchException ime) {
-                    System.out.println("Intentalo de nuevo tontito");
-                    sc.nextLine();
                 }
-            } while (salida);
+                break;
+            } catch (InputMismatchException ime) {
+                System.out.println("El dato tiene que ser numerico");
+                sc.nextLine();
+            }
+        } while (true);
+        return opcion;
+    }
 
-            if (opcion == 1) {
-// SIGUIENTE GENERACION --------------------------------------------------------
-                
-                
-                
-            } else {
-// MENU 3 (GUARDAR PARTIDA)-----------------------------------------------------
-                String menu3 = """
+    public static int menu3() {
+        Scanner sc = new Scanner(System.in);
+        String menu3 = """
                        -------------------------------------------------
                        |               Juego de la vida                |
                        |               ¿Guardar Partida?               |
@@ -157,37 +146,67 @@ public class Main {
                        |                    2.- No                     |
                        |                                               |
                        -------------------------------------------------
-                      """;
-                salida = true;  // reutilizamos salida
-                opcion = 0;
-                sc.nextLine(); //limpiamos scanner
-                do {
-                    System.out.println(menu3);
-
-                    try {
-                        opcion = sc.nextInt();
-
-                        if (opcion > 0 && opcion < 3) {
-                            salida = false;
-                        } else {
-                            System.out.println("Intentalo de nuevo tontito");
-                        }
-                    } catch (InputMismatchException ime) {
-                        System.out.println("Intentalo de nuevo tontito");
-                        sc.nextLine();
-                    }
-                } while (salida);
-
-                if (opcion == 1) {
-// GUARDAR PARTIDA -------------------------------------------------------------
-                    System.out.println("mantenimiento");
+                       Respuesta: """;
+        int opcion;
+        do {
+            try {
+                System.out.print(menu3);
+                opcion = sc.nextInt();
+                while (opcion < 1 || opcion > 2) {
+                    System.out.println("El dato tiene que ser 1 o 2");
+                    System.out.print(menu3);
+                    opcion = sc.nextInt();
                 }
-
-                System.out.println("Gracias por jugar");
-                salidaJuego = true;
+                break;
+            } catch (InputMismatchException ime) {
+                System.out.println("El dato tiene que ser numerico");
+                sc.nextLine();
             }
+        } while (true);
+        return opcion;
+    }
 
-        } while (!salidaJuego);
+    public static int pregTamanio() {
+        Scanner sc = new Scanner(System.in);
+        int tamaño;
+        String pregunta = "Elige el tamaño del tablero (max 25): ";
+        do {
+            try {
+                System.out.print(pregunta);
+                tamaño = sc.nextInt();
+                while (tamaño > 25 || tamaño < 1) {
+                    System.out.println("El dato tiene que ser entre 1 y 25");
+                    System.out.print(pregunta);
+                    tamaño = sc.nextInt();
+                }
+                break;
+            } catch (InputMismatchException ime) {
+                System.out.println("El dato tiene que ser numerico");
+                sc.nextLine();
+            }
+        } while (true);
+        return tamaño;
+    }
 
+    public static int pregPorcentaje() {
+        Scanner sc = new Scanner(System.in);
+        int porcentaje;
+        String pregunta = "Ahora elija el porcentaje de celulas vivas: ";
+        do {
+            try {
+                System.out.print(pregunta);
+                porcentaje = sc.nextInt();
+                while (porcentaje > 100 || porcentaje < 1) {
+                    System.out.println("El dato tiene que ser entre 1 y 100");
+                    System.out.print(pregunta);
+                    porcentaje = sc.nextInt();
+                }
+                break;
+            } catch (InputMismatchException ime) {
+                System.out.println("El dato tiene que ser numerico");
+                sc.nextLine();
+            }
+        } while (true);
+        return porcentaje;
     }
 }
