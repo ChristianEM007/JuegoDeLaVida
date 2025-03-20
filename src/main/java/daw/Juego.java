b830c47eb9b3bd697cb24a5acd4a40100d551b77
ChristianEysel <ceysmar2109@g.educaand.es>
2025-03-20 09:03:49 +0100

    clase juego terminado

--- src/main/java/daw/Juego.java
+++ src/main/java/daw/Juego.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package daw;
 
 /**
@@ -11,7 +8,7 @@ package daw;
 public class Juego {
     
     private Celula[][] tablero;
-
+    
     public Juego(int tamano) {
         this.tablero = new Celula[tamano][tamano];
     }
@@ -23,7 +20,61 @@ public class Juego {
     public void revivirCelula(int fila, int columna){
         tablero[fila][columna].vida();
     }
-
+    
+    public void recorrerTablero(){
+        for (int i = 0; i <= tablero.length; i++) {
+            for (int j = 0; j <= tablero[0].length; j++) {
+                if(tablero[i][j].isViva()){
+                    if(mementoMori(i, j)){
+                        tablero[i][j].muerte();
+                    }
+                }else{
+                    if(comprobarRevivicion(i, j)){
+                        tablero[i][j].vida();
+                    }
+                }
+            }
+        }
+    }
+    
+    public int comprobarVecinas(int fila, int columna){
+        
+        int cantidad = 0;
+        
+        for (int i = fila-1; i <= fila + 1; i++) {
+            for (int j = columna-1; j <= columna + 1; j++) {
+                
+                if(fila != i && columna != j){
+                    if(tablero[i][j].isViva()){
+                        cantidad++;
+                    }
+                }
+            }
+        }
+        return cantidad;
+    }
+    
+    public boolean comprobarRevivicion(int fila, int columna){
+        
+        int vecinas = comprobarVecinas(fila, columna);
+        
+        return vecinas == 3;
+    }
+    
+    public boolean mementoMori(int fila, int columna){
+        
+        boolean morir = true;
+        int vecinas = comprobarVecinas(fila, columna);
+        
+        if(vecinas <= 1 ){
+            return morir;
+        }else if(vecinas > 3){
+            return morir;
+        }
+        
+        return false;
+    }
+    
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
